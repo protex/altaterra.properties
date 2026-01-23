22447e7f4827f36ed0c83c68ed908d4c9fb04c6f
Peter Maggio <pmaggio@Mac.lan>
2026-01-23 16:55:55 -0600

    Add wholesale page

---
+++ assets/js/wholesale.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $(window).on("scroll", function(){
+        if(window.scrollY > 200){
+            $("#scroll_top").show();
+        } else {
+            $("#scroll_top").hide();
+        }
+
+});
+
+    // Fetch properties JSON data
+    fetch('./assets/wholesale-properties.json')
+        .then(response => response.json())
+        .then(data => {
+            propertiesData = data;
+            // Render cards
+            propertiesData.forEach(property => {
+                let cardHtml = `<div class="col-6 col-lg-3 my-5 m-0 m-auto">
+                    <div class="card">
+                        <div class="img_conta">
+                          <a href="/property-page?property=${property.name}" target="_blank">
+                            <img class="card-img-top" src="${property.image}" alt="Card image cap">
+                          </a>
+                          <div class="card-price">${property.price}</div>
+                        </div>
+                        <div class="card-body d-flex justify-content-between">
+                            <div>
+                                <h5 class="card-title">${property.title}</h5>
+                                <p class="card-text">${property.location}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+                $('#picture-view-content').append(cardHtml);
+            });
+        });
+
+    // Scroll to top animation on button click
+    $("#scroll_top").click(function(){
+        $('html, body').animate({scrollTop: 0}, 50);
+    });
+});
